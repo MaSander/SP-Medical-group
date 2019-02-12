d952f5ad406208def24eb070e7aec9baa57511c3
Marcelo Sander Mariano Guimarães <51755101856@info.local>
2019-02-12 15:52:06 -0200

    finalização

--- SCRIPT_DML_SP_MEDICAL_GROUP_02.sql
+++ SCRIPT_DML_SP_MEDICAL_GROUP_02.sql
@@ -33,16 +33,6 @@ from 'C:\db\MEDICOS.csv'
 		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
 );
 
-bulk insert MEDICOS
-from 'C:\db\MEDICOS.csv'
-	with(
-		format = 'csv',
-		firstrow = 2, -- primeira linha de dados = 2
-		fieldterminator = ';', -- separador de campos = ';'
-		rowterminator = '\n', -- separador de linhas = '\n'
-		CODEPAGE = 'ACP', -- codificação dos dados = 'ACP'
-		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
-);
 
 bulk insert PRONTUARIOS
 from 'C:\db\PRONTUARIOS.csv'
@@ -55,16 +45,6 @@ from 'C:\db\PRONTUARIOS.csv'
 		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
 );
 
-bulk insert PRONTUARIOS
-from 'C:\db\PRONTUARIOS.csv'
-	with(
-		format = 'csv',
-		firstrow = 2, -- primeira linha de dados = 2
-		fieldterminator = ';', -- separador de campos = ';'
-		rowterminator = '\n', -- separador de linhas = '\n'
-		CODEPAGE = 'ACP', -- codificação dos dados = 'ACP'
-		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
-);
 
 bulk insert CONSULTA
 from 'C:\db\CONSULTAS.csv'
@@ -81,13 +61,24 @@ SELECT * FROM PRONTUARIOS
 
 SELECT * FROM CONSULTA
 
-SELECT * FROM USUARIOS
+SELECT * FROM CLINICAS
 
 -------------------------------------
 
 DELETE FROM USUARIOS
 DBCC CHECKIDENT ('USUARIOS', RESEED, 0)
 
+DELETE FROM MEDICOS
+DBCC CHECKIDENT ('MEDICOS', RESEED, 0)
 DELETE FROM PRONTUARIOS
-
+DBCC CHECKIDENT ('PRONTUARIOS', RESEED, 0)
 DELETE FROM CONSULTA
+DBCC CHECKIDENT ('CONSULTA', RESEED, 0)
+
+---------------------------------------
+
+SELECT * FROM MEDICOS
+SELECT * FROM PRONTUARIOS
+SELECT * FROM CONSULTA
+
+----------------------------------------
